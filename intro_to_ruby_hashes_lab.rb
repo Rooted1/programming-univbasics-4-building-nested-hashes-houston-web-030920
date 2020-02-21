0f8dc35a78ec2cecb71da4e067728b1d0239cf2b
Ruth Obe <ruthobey@gmail.com>
2020-02-21 04:38:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,7 +14,12 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
-  
+  # keyword "return." Nice and neat.
+	{
+	  :railroads => {
+	    :pieces => ""
+	  }
+	}
 end
 
 def monopoly_with_third_tier
