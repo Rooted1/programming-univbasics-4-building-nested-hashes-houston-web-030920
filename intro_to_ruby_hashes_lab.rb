1f88a9beddca783b4497185683d109951fb354f2
Ruth Obe <ruthobey@gmail.com>
2020-02-21 04:44:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,7 +32,7 @@ def monopoly_with_third_tier
 	    :pieces => 4,
 	    :rent_in_dollars => {
 	      ::one_piece_owned => "",
-	      :key_2 => "",
+	      ::one_piece_owned => "",
 	      :key_3 => "",
 	      :key_4 => ""
 	    }
